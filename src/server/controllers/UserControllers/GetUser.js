f296aa19c9b68cebc48dc7984d1868659db895a8
Diogo Santana <diogosantanafreitasuna@gmail.com>
2023-10-22 22:50:11 -0300

    Corrigindo bug de requisição no Redis

--- src/server/controllers/UserControllers/GetUser.js
+++ src/server/controllers/UserControllers/GetUser.js
@@ -1,39 +1,46 @@
-const {Usuario} = require('../../models/User.js');
-const client = require('../../database/redisConnection.js')
+const { Usuario } = require('../../models/User.js');
+const client = require('../../database/redisConnection.js');
 
-async function getUser (req, res) {
+async function getUser(req, res) {
     let usuario = null;
     try {
         usuario = await client.get(req.params.email);
-        if(usuario)
-            console.log('pegou o cache')
+        if (usuario) {
+            console.log('pegou o cache');
             res.status(200).send(usuario);
-        usuario = await Usuario.findOne({ where: { email: req.params.email } });
-        if (usuario) 
-            console.log('fez a requisição')
-            client.setEx(req.params.email, 200, JSON.stringify(usuario));
-            res.status(200).send(usuario);
-    } catch {
-        res.status(404).send('User not found!')
+        } else {
+            usuario = await Usuario.findOne({ where: { email: req.params.email } });
+            if (usuario) {
+                console.log('fez a requisição');
+                client.setEx(req.params.email, 200, JSON.stringify(usuario));
+                res.status(200).send(usuario);
+            } else {
+                res.status(404).send('User not found!');
+            }
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
     }
 }
 
-async function getUsers (req, res) {
+async function getUsers(req, res) {
     let users = null;
-    try{
+    try {
         users = await client.get('users');
-        if(users)
-            console.log('pegou o cache')
+        if (users) {
+            console.log('pegou o cache');
             res.status(200).send(users);
-        users = await Usuario.findAll();
-        console.log('fez a requisição')
-        //client.setEx('users', 100, JSON.stringify(users));
-        res.status(200).send(users);
-    } catch(err) {
-        res.send(err);
+        } else {
+            users = await Usuario.findAll();
+            console.log('fez a requisição');
+            client.setEx('users', 100, JSON.stringify(users));
+            res.status(200).send(users);
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
     }
 }
 
-
-
-module.exports = {getUser, getUsers}
+module.exports = { getUser, getUsers };
